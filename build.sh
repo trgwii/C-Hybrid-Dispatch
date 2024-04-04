b53965ce91086c4a7a237d05dca00c95234b22ed
Thomas Rory Gummerson <thomas@gummerson.no>
2024-04-04 11:39:04 +0200

    Show flattened main as well

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 set -e
 zig cc -Weverything -Werror -O2 -shared giraffe.c -lc -o libgiraffe.so
+zig cc -Weverything -Werror -Wno-unsafe-buffer-usage -Wno-switch-enum -O2 main.c cat.c dog.c animal.c -lc -o main
+objdump -M intel --disassembler-color=color --visualize-jumps=extended-color --no-addresses --no-show-raw-insn --section=.text --disassemble=main -S main
+./main
 zig cc -Weverything -Werror -Wno-unsafe-buffer-usage -Wno-switch-enum -DDISPLAY_ASM -O2 main.c cat.c dog.c animal.c -lc -o main
 objdump -M intel --disassembler-color=color --visualize-jumps=extended-color --no-addresses --no-show-raw-insn --section=.text --disassemble=Animal_vocalize main
-zig cc -Weverything -Werror -Wno-unsafe-buffer-usage -Wno-switch-enum -O2 main.c cat.c dog.c animal.c -lc -o main
-./main
