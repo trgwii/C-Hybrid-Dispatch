72e934240862e98eefc19f19c45fd4a66bffca53
Thomas Rory Gummerson <thomas@gummerson.no>
2024-04-04 10:43:03 +0200

    Fix objdump flags

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 zig build-lib -O ReleaseFast -dynamic giraffe.c -lc
 zig build-exe -O ReleaseFast main.c cat.c dog.c animal.c -lc
 ./main
-objdump --disassemble=Animal_vocalize main
+objdump -M intel --disassembler-color=color --disassemble=Animal_vocalize main
